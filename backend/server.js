e299ffd5e74126915460a74b2dc13a2fdcbf6868
pritam <pritam.saha@webskitters.com>
2021-07-25 23:07:39 +0530

    seeder

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import colors from "colors";
 import products from './data/products.js'
+import connectDB from './config/db.js'
 // const express=require('express');
 // const products=require('./data/products');
 
 dotenv.config()
+connectDB()
 const app=express()
 app.get('/',(req,res)=>{
     res.send('API is running ....')
@@ -16,5 +19,5 @@ app.get("/api/products/:id", (req, res) => {
     const product=products.find(p=>p._id===req.params.id)
   res.json(product);
 });
-const PORT=process.env.PORT ||5000 //test
-app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+const PORT=process.env.PORT ||5000 
+app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
