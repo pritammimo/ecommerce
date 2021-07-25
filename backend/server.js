af2699d135dd90959cc452cec92a85387e93f940
pritam <pritam.saha@webskitters.com>
2021-07-25 22:04:58 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -16,5 +16,5 @@ app.get("/api/products/:id", (req, res) => {
     const product=products.find(p=>p._id===req.params.id)
   res.json(product);
 });
-const PORT=process.env.PORT ||5000
+const PORT=process.env.PORT ||5000 //test
 app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
