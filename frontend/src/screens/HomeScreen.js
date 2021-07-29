f5b92fac660aad6a4790a1aca62d4288e083006e
pritam <pritam.saha@webskitters.com>
2021-07-29 22:58:27 +0530

    Product details api add

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,27 +1,30 @@
 import React,{useState,useEffect} from 'react'
 import { Col, Row } from 'react-bootstrap'
 import Product from '../components/Product'
-// import products from '../products'
-import axios from 'axios';
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { useSelector,useDispatch } from 'react-redux';
+import {listProducts} from '../actions/productActions'
 const HomeScreen = () => {
-    const [products, setProducts] = useState([])
+    const dispatch=useDispatch()
+    const productList=useSelector(state=>state.productList)
+    const {loading,error,products}=productList
+    // const [products, setProducts] = useState([])
     useEffect(() => {
-        const fetchProducts=async()=>{
-            const {data}=await axios.get('/api/products')
-             setProducts(data)
-        }
-        fetchProducts()
-    }, [])
+       dispatch(listProducts())
+    }, [dispatch])
+    // const products=[]
     return (
         <>
             <h1>Latest Product</h1>
-            <Row>
+            {loading?<h2><Loader /></h2>:error?<Message variant='danger'>{error}</Message >:<Row>
                 {products.map(product =>(
                     <Col  key={product._id} sm={12} md={6} lg={4} xl={3}>
                         <Product product={product}/>
                     </Col>
                 ))}
-            </Row>
+            </Row>}
+            
         </>
     )
 }
