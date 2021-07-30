165ddddd77298439822c89934bdeda51a0197c75
pritam <pritam.saha@webskitters.com>
2021-07-30 22:44:18 +0530

    add to cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,21 +5,21 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 const App=()=>{
   return (
     <Router>
-  
-    <Header/>
-    <main className='py-3'>
-      <Container>
-        <Route path='/' component={HomeScreen} exact/>
-        <Route path='/product/:id' component={ProductScreen} />
-      </Container>
-  
-    </main>
-    <Footer/>
- </Router>
-  )
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
