6d8d77db4521b7864d9f5ee8887204f0d3985c7d
pritam <pritam.saha@webskitters.com>
2021-08-02 22:46:32 +0530

    login screen

---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,56 @@
+import React,{useState,useEffect} from 'react'
+import {Link, useLocation} from 'react-router-dom'
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import {login} from '../actions/userActions'
+import FormContainer from '../components/FormContainer';
+import { Button, Col, Form, Row } from 'react-bootstrap';
+const LoginScreen = ({location}) => {
+  
+    const [email,setEmail]=useState('')
+     const [password, setPassword] = useState("");
+     const redirect=location.search?location.search.split('=')[1]:'/'
+ const submitHandler=()=>{
+     alert('hi')
+ }
+    return (
+      <FormContainer>
+        <h1>Sign In</h1>
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="email">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter email"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Button type='submit' variant='primary'>
+              Sign In
+          </Button>
+        </Form>
+        <Row className='py-3'>
+          <Col>
+          New Customer?{' '}
+          <Link to={redirect?`/register?redirect=${redirect}`:'/register'}>
+          Register 
+          </Link>
+          {/* 10 m 33 sec */}
+          </Col>
+        </Row>
+      </FormContainer>
+    );
+}
+
+export default LoginScreen
